2f12a8ddcc5adbcd90aabdb268f56caf20abd23f
daph-ne <72125782+daph-ne@users.noreply.github.com>
2021-02-10 12:30:49 +0530

    Create TwoSum.java

---
+++ TwoSum.java
@@ -0,0 +1,44 @@
+/**
+* 1. Two Sum
+* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+* You may assume that each input would have exactly one solution, and you may not use the same element twice.
+* You can return the answer in any order.
+*
+* Example 1:
+* Input: nums = [2,7,11,15], target = 9
+* Output: [0,1]
+* Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+*
+* Example 2:
+*
+* Input: nums = [3,2,4], target = 6
+* Output: [1,2]
+*
+* Example 3:
+* Input: nums = [3,3], target = 6
+* Output: [0,1]
+
+* Constraints:
+*       2 <= nums.length <= 103
+*      -109 <= nums[i] <= 109
+*      -109 <= target <= 109
+*       Only one valid answer exists.
+*/
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+    //Create new array with size 2 since only one valid answer exists
+        int[] result = new int[2];
+        //Loop through the array
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+            //Check if sum is target and return the indeces
+                if(nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+}
