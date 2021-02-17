413606b97e12073fe072b903f95b74a42704dbd2
daph-ne <72125782+daph-ne@users.noreply.github.com>
2021-02-17 18:41:03 +0530

    Update ErrorsAndSolutions.java

--- ErrorsAndSolutions.java
+++ ErrorsAndSolutions.java
@@ -1,5 +1,12 @@
-1. Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException
+1. Error: 
+    Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException
 Reason:
     When you try to access an invalid bound.
 Solution:
     Correcting the size of the index.
+        
+2. int[] input =[1, 100, 1000, 200, 450];
+Error:
+    illegal start of expression
+Solution:
+    int[] input = new int[]{1, 100, 1000, 200, 450};
