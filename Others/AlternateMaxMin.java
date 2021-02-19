1aa098f23ad98d3d6eccc99fb147901fb2646279
Davis Jeffrey <daph.ne@zohomail.in>
2021-02-19 11:39:49 +0530

    Link added

--- Others/AlternateMaxMin.java
+++ Others/AlternateMaxMin.java
@@ -1,4 +1,5 @@
 /**
+ *  Link - https://www.geeksforgeeks.org/zoho-interview-set-2-campus/
  *  Alternate sorting: Given an array of integers, rearrange the array in such a way that the first element is first maximum and second element is first minimum.
  *
  *     Eg.) Input  : {1, 2, 3, 4, 5, 6, 7}
