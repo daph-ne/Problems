4bb6de3254c821eaa0b532d83fbaac76fe495164
Davis Jeffrey <daph.ne@zohomail.in>
2021-02-22 22:15:37 +0530

    Created Constants class

---
+++ Others/TrainBookings/Constants.java
@@ -0,0 +1,13 @@
+package trainbookings;
+
+public class Constants {
+    static final int UNRESERVED = 1;
+    static final int SLEEPER = 2;
+    static final int AIR_CONDITIONED = 3;
+    static final String LOWER_BERTH = "L";
+    static final String UPPER_BERTH = "U";
+    static final int LIST_OF_COACHES = 1;
+    static final int BOOK_TICKETS = 2;
+    static final int CANCEL_TICKETS = 3;
+    static final int EXIT = 4;
+}
