e42c7b12bb65a1b7593e20247f968753e439a3af
Davis Jeffrey <daph.ne@zohomail.in>
2021-02-23 09:48:31 +0530

    Minor formatting

--- Others/TrainBookings/TrainBooking.java
+++ Others/TrainBookings/TrainBooking.java
@@ -1,41 +1,25 @@
-package TrainBookings;
+package trainbookings;
 
 import java.util.Scanner;
 
 public class TrainBooking {
-    int totalUR = 20;
-    int totalSL = 10;
-    int totalAC = 10;
 
-    int totalURLower = 10;
-    int totalURUpper = 10;
-    int totalSLLower = 5;
-    int totalSLUpper = 5;
-    int totalACLower = 5;
-    int totalACUpper = 5;
+    Unreserved ur;
+    Sleeper sl;
+    AirConditioned ac;
 
-    static int availURLower = 10;
-    static int availURUpper = 10;
-    static int availSLLower = 5;
-    static int availSLUpper = 5;
-    static int availACLower = 5;
-    static int availACUpper = 5;
-
-    final int UNRESERVED = 1;
-    final int SLEEPER = 2;
-    final int AIR_CONDITIONED = 3;
-    static final int LIST_OF_COACHES = 1;
-    static final int BOOK_TICKETS = 2;
-    static final int CANCEL_TICKETS = 3;
-    static final int EXIT = 4;
+    public TrainBooking() {
+        ur = new Unreserved();
+        sl = new Sleeper();
+        ac = new AirConditioned();
+    }
 
     private void list() {
 
         System.out.println("LIST OF COACHES :");
-        System.out.println("    1. Unreserved (UR) - " + availURLower + "L and " + this.availURUpper  + "U seats out of " + totalUR + " seats are available.");
-        System.out.println("    2. Sleeper (SL) - " + this.availSLLower + "L and " + this.availSLUpper  + "U seats out of " + totalSL + " seats are available.");
-        System.out.println("    3. Air-Conditioned (AC)- " + this.availACLower + "L and " + this.availACUpper  + "U seats out of " + totalAC + " seats are available.");
-        System.out.println("Click enter to continue.");
+        ur.getAvailableSeats();
+        sl.getAvailableSeats();
+        ac.getAvailableSeats();
     }
 
     private void bookTickets() {
@@ -51,84 +35,12 @@ public class TrainBooking {
         book.nextLine();
         System.out.println("Enter berth - Lower(L) or Upper(U)");
         String berth = book.nextLine();
-        if (option == UNRESERVED) {
-            if (berth.equals("L")) {
-                if (numberOfSeats > availURLower) {
-                    System.out.println("Sorry, only " + availURLower + " seats are left in Lower berth.");
-                    if (availURUpper > 0 && availURUpper <= totalURUpper) {
-                        System.out.println(availURUpper + " seats are available in Upper berth.");
-                    }
-                } else if (numberOfSeats > 0 && numberOfSeats <= totalURLower) {
-                    System.out.println(numberOfSeats + " seat(s) have been booked in Lower berth.");
-                    availURLower = availURLower - numberOfSeats;
-                } else {
-                    System.out.println("Please book valid number of seats");
-                }
-            } else if (berth.equals("U")) {
-                if (numberOfSeats > availURUpper) {
-                    System.out.println("Sorry, only " + availURUpper + " seats are left in Lower berth.");
-                    if (availURLower > 0 && availURLower <= totalURLower) {
-                        System.out.println(availURLower + " seats are available in Lower berth.");
-                    }
-                } else if (numberOfSeats > 0 && numberOfSeats <= totalURUpper) {
-                    System.out.println(numberOfSeats + " seat(s) have been booked in Upper berth.");
-                    availURUpper = availURUpper - numberOfSeats;
-                } else {
-                    System.out.println("Please book valid number of seats");
-                }
-            }
-        } else if (option == SLEEPER) {
-            if (berth.equals("L")) {
-                if (numberOfSeats > this.availSLLower) {
-                    System.out.println("Sorry, only " + this.availSLLower + " seats are left in Lower berth.");
-                    if (this.availSLUpper > 0 && this.availSLUpper <= totalSLUpper) {
-                        System.out.println(this.availURUpper + " seats are available in Upper berth.");
-                    }
-                } else if (numberOfSeats > 0 && numberOfSeats <= totalSLLower) {
-                    System.out.println(numberOfSeats + " seat(s) have been booked in Lower berth.");
-                    this.availSLLower = availSLLower - numberOfSeats;
-                } else {
-                    System.out.println("Please book valid number of seats");
-                }
-            } else if (berth.equals("U")) {
-                if (numberOfSeats > this.availSLUpper) {
-                    System.out.println("Sorry, only " + this.availSLUpper + " seats are left in Lower berth.");
-                    if (this.availSLLower > 0 && this.availSLLower <= totalSLLower) {
-                        System.out.println(this.availSLLower + " seats are available in Lower berth.");
-                    }
-                } else if (numberOfSeats > 0 && numberOfSeats <= totalSLUpper) {
-                    System.out.println(numberOfSeats + " seat(s) have been booked in Upper berth.");
-                    this.availSLUpper = availSLUpper - numberOfSeats;
-                } else {
-                    System.out.println("Please book valid number of seats");
-                }
-            }
-        } else if (option == AIR_CONDITIONED) {
-            if (berth.equals("L")) {
-                if (numberOfSeats > this.availACLower) {
-                    System.out.println("Sorry, only " + this.availACLower + " seats are left in Lower berth.");
-                    if (this.availACUpper > 0 && this.availACUpper <= totalACUpper) {
-                        System.out.println(this.availACUpper + " seats are available in Upper berth.");
-                    }
-                } else if (numberOfSeats > 0 && numberOfSeats <= totalACLower) {
-                    System.out.println(numberOfSeats + " seat(s) have been booked in Lower berth.");
-                    this.availACLower = availACLower - numberOfSeats;
-                } else {
-                    System.out.println("Please book valid number of seats");
-                }
-            } else if (berth.equals("U")) {
-                if (numberOfSeats > this.availACUpper) {
-                    System.out.println("Sorry, only " + this.availACUpper + " seats are left in Lower berth.");
-                    if (this.availACLower > 0 && this.availACLower <= totalACLower) {
-                        System.out.println(this.availACLower + " seats are available in Lower berth.");
-                    }
-                } else if (numberOfSeats > 0 && numberOfSeats <= totalACUpper) {
-                    System.out.println(numberOfSeats + " seat(s) have been booked in Upper berth.");
-                    this.availACUpper = availACUpper - numberOfSeats;
-                } else {
-                    System.out.println("Please book valid number of seats");
-                }
-            }
+        if (option == Constants.UNRESERVED) {
+            ur.book(berth, numberOfSeats);
+        } else if (option == Constants.SLEEPER) {
+            sl.book(berth, numberOfSeats);
+        } else if (option == Constants.AIR_CONDITIONED) {
+            ac.book(berth, numberOfSeats);
         }
     }
 
@@ -144,78 +56,12 @@ public class TrainBooking {
         cancel.nextLine();
         System.out.println("Enter berth - Lower(L) or Upper(U)");
         String berth = cancel.nextLine();
-        if (option == UNRESERVED) {
-            if (berth.equals("L")) {
-                this.availURLower = availURLower + numberOfSeats;
-                if (this.availURLower <= totalURLower && this.availURLower > 0) {
-                    System.out.println(numberOfSeats + " seat(s) have been cancelled in Lower berth.");
-                } else if (this.availURLower > totalURLower) {
-                    System.out.println("Please check and confirm the number of seats.");
-                    this.availURLower = availURLower - numberOfSeats;
-                } else {
-                    System.out.println("Please cancel valid number of seats");
-                    this.availURLower = availURLower - numberOfSeats;
-                }
-            } else if (berth.equals("U")) {
-                this.availURUpper = availURUpper + numberOfSeats;
-                if (this.availURUpper <= totalURUpper && this.availURUpper > 0) {
-                    System.out.println(numberOfSeats + " seat(s) have been cancelled in Lower berth.");
-                } else if (this.availURUpper > totalURUpper) {
-                    System.out.println("Please check and confirm the number of seats.");
-                    this.availURUpper = availURUpper - numberOfSeats;
-                } else {
-                    System.out.println("Please cancel valid number of seats");
-                    this.availURUpper = availURUpper - numberOfSeats;
-                }
-            }
-        } else if (option == SLEEPER) {
-            if (berth.equals("L")) {
-                this.availSLLower = availSLLower + numberOfSeats;
-                if (this.availSLLower <= totalSLLower && this.availSLLower > 0) {
-                    System.out.println(numberOfSeats + " seat(s) have been cancelled in Lower berth.");
-                } else if (this.availSLLower > totalSLLower) {
-                    System.out.println("Please check and confirm the number of seats.");
-                    this.availSLLower = availSLLower - numberOfSeats;
-                } else {
-                    System.out.println("Please cancel valid number of seats");
-                    this.availURLower = availSLLower - numberOfSeats;
-                }
-            } else if (berth.equals("U")) {
-                this.availSLUpper = availSLUpper + numberOfSeats;
-                if (this.availSLUpper <= totalSLUpper && this.availSLUpper > 0) {
-                    System.out.println(numberOfSeats + " seat(s) have been cancelled in Lower berth.");
-                } else if (this.availSLUpper > totalSLUpper) {
-                    System.out.println("Please check and confirm the number of seats.");
-                    this.availSLUpper = availSLUpper - numberOfSeats;
-                } else {
-                    System.out.println("Please cancel valid number of seats");
-                    this.availSLUpper = availSLUpper - numberOfSeats;
-                }
-            }
-        } else if (option == AIR_CONDITIONED) {
-            if (berth.equals("L")) {
-                this.availACLower = availACLower + numberOfSeats;
-                if (this.availACLower <= totalACLower && this.availACLower > 0) {
-                    System.out.println(numberOfSeats + " seat(s) have been cancelled in Lower berth.");
-                } else if (this.availACLower > totalACLower) {
-                    System.out.println("Please check and confirm the number of seats.");
-                    this.availACLower = availACLower - numberOfSeats;
-                } else {
-                    System.out.println("Please cancel valid number of seats");
-                    this.availACLower = availACLower - numberOfSeats;
-                }
-            } else if (berth.equals("U")) {
-                this.availACUpper = availACUpper + numberOfSeats;
-                if (this.availACUpper <= totalACUpper && this.availACUpper > 0) {
-                    System.out.println(numberOfSeats + " seat(s) have been cancelled in Lower berth.");
-                } else if (this.availACUpper > totalACUpper) {
-                    System.out.println("Please check and confirm the number of seats.");
-                    this.availACUpper = availACUpper - numberOfSeats;
-                } else {
-                    System.out.println("Please cancel valid number of seats");
-                    this.availACUpper = availACUpper - numberOfSeats;
-                }
-            }
+        if (option == Constants.UNRESERVED) {
+            ur.cancel(berth, numberOfSeats);
+        } else if (option == Constants.SLEEPER) {
+            sl.cancel(berth, numberOfSeats);
+        } else if (option == Constants.AIR_CONDITIONED) {
+            ac.cancel(berth, numberOfSeats);
         }
     }
 
@@ -228,20 +74,19 @@ public class TrainBooking {
             System.out.println("    4. Exit");
             Scanner train = new Scanner(System.in);
             int choice = train.nextInt();
-            if (choice == LIST_OF_COACHES) {
+            if (choice == Constants.LIST_OF_COACHES) {
                 TrainBooking tr = new TrainBooking();
                 tr.list();
                 train.nextLine();
-                String option1 = train.nextLine();
 
-            } else if (choice == BOOK_TICKETS) {
+            } else if (choice == Constants.BOOK_TICKETS) {
                 TrainBooking tr = new TrainBooking();
                 tr.bookTickets();
 
-            } else if (choice == CANCEL_TICKETS) {
+            } else if (choice == Constants.CANCEL_TICKETS) {
                 TrainBooking tr = new TrainBooking();
                 tr.cancelTickets();
-            } else if (choice == EXIT) {
+            } else if (choice == Constants.EXIT) {
                 System.out.println("THANK YOU..!!");
                 break;
 
