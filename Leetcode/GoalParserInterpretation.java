ae4cae4e46993b2461ec6b5a5da3b6fad455b8c3
Davis Jeffrey <daph.ne@zohomail.in>
2021-03-27 11:00:08 +0530

    Initial commit

---
+++ Leetcode/GoalParserInterpretation.java
@@ -0,0 +1,54 @@
+/**
+ *  Link - https://leetcode.com/problems/goal-parser-interpretation/
+ *  You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+ *
+ * Given the string command, return the Goal Parser's interpretation of command.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: command = "G()(al)"
+ * Output: "Goal"
+ * Explanation: The Goal Parser interprets the command as follows:
+ * G -> G
+ * () -> o
+ * (al) -> al
+ * The final concatenated result is "Goal".
+ *
+ * Example 2:
+ *
+ * Input: command = "G()()()()(al)"
+ * Output: "Gooooal"
+ *
+ * Example 3:
+ *
+ * Input: command = "(al)G(al)()()G"
+ * Output: "alGalooG"
+ *
+ *
+ *
+ * Constraints:
+ *
+ *     1 <= command.length <= 100
+ *     command consists of "G", "()", and/or "(al)" in some order.
+ */
+public class GoalParserInterpretation {
+    public String interpret(String command) {
+        String output = "";
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == 'G') {
+                output += "G";
+            } else if (command.charAt(i) == '(') {
+                if(command.charAt(i + 1) == ')') {
+                    output += "o";
+                    i++;
+                } else if (command.charAt(i + 1) == 'a') {
+                    output += "al";
+                    i = i + 3;
+                }
+            }
+        }
+        return output;
+    }
+}
