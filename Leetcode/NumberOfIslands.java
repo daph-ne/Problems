e7cffa6fc22db7475a922bb695dcb9e6df2fe265
Davis Jeffrey <daph.ne@zohomail.in>
2021-04-18 22:04:17 +0530

    Time complexity added

--- Leetcode/NumberOfIslands.java
+++ Leetcode/NumberOfIslands.java
@@ -1,5 +1,7 @@
 /**
  * Link - https://leetcode.com/problems/number-of-islands/
+ * Time Complexity - O(n^2)
+ *
  * Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
  *
  * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
